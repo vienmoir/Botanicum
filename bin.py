091e4cb25bea950a32616807be853833953c01a0
Eclair <Eclair257@gmail.com>
2017-10-09 20:16:38 +0300

    Lul
    Lul

--- bin.py
+++ bin.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('4.jpg')
+img = cv2.imread('img/4.jpg')
 grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ######## otsu binarization ########
 retval3, otsu = cv2.threshold(grayscaled, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
@@ -21,7 +21,7 @@ for i in range(0, nb_components):
 
 ###### closing small holes inside the foreground objects or small black points on the object ##########
 closing = cv2.morphologyEx(img2, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT,(15,15)))
-#cv2.imshow('closing', closing)
+cv2.imshow('closing', closing)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
