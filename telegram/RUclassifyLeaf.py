46c38ab02ffc5f6d08cd2b0af5f028d210ccc7e9
vienmoir <daria.korotayeva@yandex.ru>
2017-12-03 02:34:27 +0300

    Только русскоязычный бот

--- telegram/RUclassifyLeaf.py
+++ telegram/RUclassifyLeaf.py
@@ -9,16 +9,17 @@ import numpy as np
 import pandas as pd
 
 def classify(data):
-    mms = pd.read_pickle('RUmms.p',compression='infer')
     data = data.drop(['Mode','Vertical_symmetry','Horizontal_symmetry', 'Minimal_peak'], axis=1)
+    mms = pd.read_pickle('RUmms.p',compression='infer')
     example = data.iloc[0:1,0:data.shape[1]]
     mms = pd.concat([mms,example],ignore_index=True)
     example_scaled = ((mms-mms.min())/(mms.max()-mms.min())).loc[2:2,]
 
     ### загрузка модели
     filename = 'RUmodel.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-
+    loaded_model = pickle.load(open(filename, 'rb'))    
+    
+    ### загрузка названий классов
     filehandler = open('RUclasses.obj', 'r') 
     classes = pickle.load(filehandler)
     
