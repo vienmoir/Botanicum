e79f20cb9d46dae69efbc9bb93aa02569a3f199f
vienmoir <daria.korotayeva@yandex.ru>
2017-12-02 23:11:45 +0300

    Пытаюсь сделать ветку с англоязычным ботом

--- telegram/classifyLeaf.py
+++ telegram/classifyLeaf.py
@@ -10,18 +10,17 @@ import pandas as pd
 
 def classify(data):
     mms = pd.read_pickle('mms.p',compression='infer')
+    data = data.drop(['Mode','Vertical_symmetry','Horizontal_symmetry', 'Minimal_peak'], axis=1)
     example = data.iloc[0:1,0:data.shape[1]]
     mms = pd.concat([mms,example],ignore_index=True)
     example_scaled = ((mms-mms.min())/(mms.max()-mms.min())).loc[2:2,]
 
     ### загрузка модели
-    filename = '3nnmodel.sav'
+    filename = 'model.sav'
     loaded_model = pickle.load(open(filename, 'rb'))
-    # если всё-таки 1 результат:
-    #result = loaded_model.predict(example)    
-    
+
     ### загрузка названий классов
-    filehandler = open('20classes.obj', 'r') 
+    filehandler = open('classes.obj', 'r') 
     classes = pickle.load(filehandler)
     
     ### TOP-3 (TOP-2 ИЛИ TOP-1)
@@ -29,9 +28,11 @@ def classify(data):
     ind = np.argpartition(prob[0], -3)[-3:]
     ind = ind[np.argsort(prob[0][ind])]
     ind = [item for item in ind if prob[0][item] > 0]
-    result = classes[ind[0]]
+    result2 = 0
+    result3 = 0
+    result1 = classes[ind[0]]
     if len(ind) > 1:
-        result = classes[ind[1]]
+        result2 = classes[ind[1]]
         if len(ind) > 2:
-            result = classes[ind[2]]
-    return result
+            result3 = classes[ind[2]]
+    return result1, result2, result3
